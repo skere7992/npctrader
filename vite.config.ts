eeea7c1f21eacfe51edc1aca0299c4c50cfcd733
manuel ignacio soto fiora del fabro <167094803+skere7992@users.noreply.github.com>
2025-07-28 00:57:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/', // Configurable base path
+  base: '/npctrader/', // Ruta base para GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+VITE_BASE_PATH=/npctrader/
+npm run build
